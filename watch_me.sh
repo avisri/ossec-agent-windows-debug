cb1ae1ed7777559b4ed5f83c6646b637e7c35255
unknown <avinash.sridhar@rbains-7240-A2.internal.salesforce.com>
2015-09-16 03:24:34 -0700

    rearrange watch  me commands

--- watch_me.sh
+++ watch_me.sh
@@ -1,25 +1,24 @@
-#!/bin/bash 
-
+#!/bin/bash
 
 echo '----------------------Server Info ---------------------------------'
-./grep.exe Using ../ossec.log | tail -2
+grep.exe Using ../ossec.log | tail -2
 echo
 echo '----------------------Agent Process status ------------------------'
 #this doesn't work as bash reads /V as V:/
 #tasklist /V | ./grep.exe  ossec-agent.exe
-tasklist  | ./grep.exe  ossec-agent.exe
+tasklist  | grep.exe  ossec-agent.exe
 echo
 echo '----------------------Rids counter Status -------------------------'
-./grep.exe . 	../rids/*
+grep.exe . 	../rids/*
+echo
+echo
+echo
+echo "                           Failures                                 "
+echo '---------------------------------------------------------------------'
+grep 'Unable to send' ../ossec.log | tail -2 
+echo '--------'
+echo 'Total:' 
+grep -c  'Unable to send' ../ossec.log
 echo
 echo '--------------------- Sample last three lines of logs -------------'
-./tail.exe -3 	../ossec.log
-echo 
-echo 
-echo "                           Failures                                 "
-echo ---------------------------------------------------------------------
-grep 'Unable to send' ../ossec.log | tail -2 
-echo --------
-echo Total: 
-grep -c  'Unable to send' ../ossec.log
-
+tail.exe -3 	../ossec.log
